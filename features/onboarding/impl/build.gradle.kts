9d053a6a5d97a09290e819a871e51ca311c3cbb4
Alekseo <fedosenok@gmail.com>
2024-04-03 22:38:04 +0200

    Поправки по слиянию с Dev

--- features/onboarding/impl/build.gradle.kts
+++ features/onboarding/impl/build.gradle.kts
@@ -8,6 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
+
         minSdk = 24
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -59,7 +60,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+//    testImplementation(libs.junit)
+//    androidTestImplementation(libs.androidx.junit)
+//    androidTestImplementation(libs.androidx.espresso.core)
 }
